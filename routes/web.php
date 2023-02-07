06d0e5d6ef52f7f4dd0b7edc5ef55720734da94e
Lydia Ego <egolydia97@gmail.com>
2023-02-07 13:45:42 +0100

    Feat: create routes

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Models\Video;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'videos' => Video::inRandomOrder()->get()
+    ]);
+})->name('home');
+
+Route::get('/delete-video', function () {
+    return Inertia::render('DeleteVideo');
+})->name('deleteVideo');
+
+Route::get('/add-video', function () {
+    return Inertia::render('AddVideo');
+})->name('addVideo');
+
+Route::get('/videos/{id}', [\App\Http\Controllers\VideosController::class, 'show'])->name('videos.show');
+
+require __DIR__.'/auth.php';
